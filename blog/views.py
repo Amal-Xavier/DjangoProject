f474edc2c7f8a2d65d7f0ff0c5c776fc7afafd84
Amal Xavier <amalxavier41134@gmail.com>
2026-08-02 22:10:44 +0530

    Updated models, views, templates, and admin

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title' : 'Blog Post',
-        'content' : 'first post content',
-        'date_posted' : 'August 27, 2018',
-    },
-    {
-        'author': 'Jane Doe',
-        'title' : 'Blog Post 2',
-        'content' : 'second post content',
-        'date_posted' : 'August 28, 2018',
-    }
-]
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
