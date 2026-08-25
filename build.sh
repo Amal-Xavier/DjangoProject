79df70c32290f05e325efb535549408c6bfb8378
Amal Xavier <amalxavier41134@gmail.com>
2026-08-25 21:09:11 +0530

    Deployed suuccesfully

--- build.sh
+++ build.sh
@@ -5,13 +5,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py loaddata data.json
 
-# Automatically create a superuser if it doesn't already exist
-python manage.py shell << END
+# Automatically create superuser safely
+python -c "
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='AmalXavier').exists():
-    User.objects.create_superuser('AmalXavier', 'amalxavier41134@gmail.com', 'ypvp rfkn emrk ioee')
-END
-
+    User.objects.create_superuser('AmalXavier', 'amalxavier41134@gmail.com', 'YourChosenWebPassword123')
+"
