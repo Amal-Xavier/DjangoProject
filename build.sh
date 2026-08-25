866df11e0e0dba90733a840e13a30dd6d15a332b
Amal Xavier <amalxavier41134@gmail.com>
2026-08-25 20:48:50 +0530

    Add automatic superuser creation in build script

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a superuser if it doesn't already exist
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='AmalXavier').exists():
+    User.objects.create_superuser('AmalXavier', 'amalxavier41134@gmail.com', 'YourSecurePassword123')
+END
